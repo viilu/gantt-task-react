eb94bbb84faea7dec1626beffcf35d09c0fc0ae4
Sami Pussinen <email@samipussinen.com>
2023-08-14 18:18:17 +0000

    adjust month display

--- src/components/calendar/calendar.tsx
+++ src/components/calendar/calendar.tsx
@@ -131,7 +131,7 @@ export const Calendar: React.FC<CalendarProps> = ({
         <text
           key={bottomValue + date.getFullYear()}
           y={headerHeight * 0.8}
-          x={columnWidth * i + columnWidth * 0.2}
+          x={columnWidth * i + columnWidth * 0.5}
           className={styles.calendarBottomText}
         >
           {bottomValue}
